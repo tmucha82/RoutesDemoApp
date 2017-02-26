5f949e8d1692308e706b7b5e9e5803299c94e70e
tmucha <tmucha@cyfrowypolsat.pl>
2017-02-26 11:05:07 +0100

    all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import {HttpModule} from '@angular/http';
 
 import {RoutesDemoApp} from './app.component';
 import {Routes, RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  // {path: '', redirectTo: 'home', pathMatch: 'full'},
-  // {path: 'home', component: HomeComponent},
-  // {path: 'about', component: AboutComponent},
-  // {path: 'contact', component: ContactComponent},
-  // {path: 'contactus', redirectTo: 'contact'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'contactus', redirectTo: 'contact'},
 ];
 
 @NgModule({
   declarations: [
-    RoutesDemoApp
+    RoutesDemoApp,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
